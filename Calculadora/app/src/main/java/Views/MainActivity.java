4faece273834dac21712c7196e08a4502a33693e
juliagg14 <137561082+juliagg14@users.noreply.github.com>
2023-10-31 20:13:13 -0300

    Add files via upload

---
+++ Calculadora/app/src/main/java/Views/MainActivity.java
@@ -0,0 +1,39 @@
+package Views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.calculadorasimples.R;
+
+public class MainActivity extends AppCompatActivity {
+
+
+        Button Botao1;
+        Button Botao2;
+        Button Botao3;
+        Button Botao4;
+        Button Botao5;
+        Button Botao6;
+        Button Botao7;
+        Button Botao8;
+        Button Botao9;
+        Button Botao0;
+        Button BotaoMais;
+        Button BotaoMenos;
+        Button BotaoX;
+        Button BotaoDivisao;
+        Button BotaoIgual;
+
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
