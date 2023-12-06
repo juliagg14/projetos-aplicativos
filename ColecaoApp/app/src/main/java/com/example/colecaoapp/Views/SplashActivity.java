1b001917ed767e0a75d18f7ae7dcb79780101435
juliagg14 <137561082+juliagg14@users.noreply.github.com>
2023-12-06 16:53:25 -0300

    a

--- ColecaoApp/app/src/main/java/com/example/colecaoapp/Views/SplashActivity.java
+++ ColecaoApp/app/src/main/java/com/example/colecaoapp/Views/SplashActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.colecaoapp.R;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
     }
 }
